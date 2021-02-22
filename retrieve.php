c8f0f2fd520a8436de8429909f778c8bb29e1eee
Juho Vikman <c0viju00@students.oamk.fi>
2021-02-22 13:20:45 +0200

    tiedon hakeminen toteutettu

---
+++ retrieve.php
@@ -0,0 +1,11 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+
+$db = new PDO('mysql:host=localhost;dbname=todo;charset=utf8','root','');
+$sql = "select * from task";
+$query = $db->query($sql);
+$results = $query->fetchAll(PDO::FETCH_ASSOC);
+
+header('HTTP/1.1 200 OK');
+echo json_encode($results);
